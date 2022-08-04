fc5628785225cbdd0b73a90ad1eaf051006161c9
ShaoMin <shaomin.liu@holmusk.com>
2022-08-04 03:59:42 +0000

    edited find packages in setup

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 To install:
 ```
-python3 -m pip install -e "git+ssh://git@github.com/ShaoMinLiu-Holmusk/sentenceTokenizing.git@main#egg=notetosent"
+python3.8 -m pip install "git+ssh://git@github.com/ShaoMinLiu-Holmusk/sentenceTokenizing.git@main#egg=NoteToSent"
 
 python3 -m pip install -e "git+https://github.com/ShaoMinLiu-Holmusk/sentenceTokenizing.git#egg=NoteToSent"
 ```
--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ShaoMinLiu-Holmusk/sentenceTokenizing",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(
+        include=["NoteToSent", "NoteToSent.*"]
+        ),
     package_data={
         '':['*.json', '*.csv', "*.dic", "*.aff"]},
     classifiers=[
